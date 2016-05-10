0fca8ccef5cc220fde7b4ab002711176e8725591
Pluzhnikov Anton <pluzhnikov.anton@hotmail.com>
2016-05-10 18:03:07 +0300

    lab to lesson 1.8.2 init

---
+++ Lesson18/lesson2/lab1/AccessControlLisLab.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson18.lesson2.lab1
+{
+    class AccessControlLisLab
+    {
+        public static void ReadACL()
+        {
+            using (var fileStream = File.Open(@"d:\Flights.csv", FileMode.Open))
+            {
+                var fileSecurity = fileStream.GetAccessControl();                
+                //var rules = fileSecurity.GetAccessRules(true, true, typeof(NTAccount));
+                var rules = fileSecurity.GetAccessRules(true, true, typeof(SecurityIdentifier));
+                //rules = fileSecurity.gets(true, true, typeof(SecurityIdentifier));
+
+                foreach (var rule in rules)
+                {
+                    var fileRule = rule as FileSystemAccessRule;
+                    Console.WriteLine($"Access type: {fileRule.AccessControlType}");
+                    Console.WriteLine($"Rights: {fileRule.FileSystemRights}");
+                    Console.WriteLine($"Identity: {fileRule.IdentityReference}");
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
